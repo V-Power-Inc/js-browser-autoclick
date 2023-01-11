16ee1a847dd36b7d3f3d794c0ad14c292ef0a7c5
PC_Principal <basil2305@ya.ru>
2023-01-11 12:43:37 +0300

    Enabled try/catch block

--- solution.js
+++ solution.js
@@ -2,7 +2,7 @@
 setInterval(() => {
 
     /** Selectors or id which we need for click */
-    var nodes = document.getElementsByClassName('js-title-edit-button')
+    var nodes = document.getElementsByClassName('js-title-edit-button-1');
 
     /** JS click event */
     var clickEvent = new MouseEvent("click", {
@@ -11,9 +11,16 @@ setInterval(() => {
         "cancelable": false
     });
 
-    /** Selector which we want to be clicked */
-    nodes[0].dispatchEvent(clickEvent);
+    /** Try to click selector - if its not exist's, we logging in the catch block */
+    try {
+        /** Selector which we want to be clicked */
+        nodes[0].dispatchEvent(clickEvent);
 
-    /** Console output */
-    console.log('Клик 11');
+        /** Console output */
+        console.log('Клик 11');
+    } catch (TypeError) {
+
+        /** Catch type error if element not exists */
+        console.log(TypeError);
+    }
 }, 2000);
