3d1a0f3734bb64c28024786213f5b0705b797a6b
PC_Principal <basil2305@ya.ru>
2023-04-14 13:20:26 +0300

    Solution for view translations with popups (Multi modules)

--- solution.js
+++ solution.js
@@ -5,7 +5,7 @@ setInterval(() => {
     var elements_by_class = document.getElementsByClassName('js-title-edit-button-1');
 
     /** Selector of element with id (Use if selector is id) */
-    var element_by_id = document.getElementById('elem-id');
+    var element_by_id = document.getElementById('btn-confirm');
 
     /** JS click event */
     var clickEvent = new MouseEvent("click", {
@@ -17,7 +17,7 @@ setInterval(() => {
     /** Try to click selector - if its not exist's, we logging in the catch block */
     try {
         /** Selector which we want to be clicked */
-        elements_by_class[0].dispatchEvent(clickEvent);
+        element_by_id.dispatchEvent(clickEvent);
 
         /** Console output */
         console.log('Element being clicked');
